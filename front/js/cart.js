51947ff6f112b18a54d10ef942b19442ef93135b
Jérémy Verrier <jeremy.verrier2a@gmail.com>
2022-10-14 11:59:02 +0200

    mise en place de la condition ou le même objet dans le localstorage est remplacé par le plus récent

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,39 +1,34 @@
 const allCart = JSON.parse(localStorage.getItem("elementsCart"));
 console.log(allCart);
-let allCartWithInfo =[];
+let allCartWithInfo = [];
 
-async function fetchProductInCart(){
-    let promiseArray =[];
+async function fetchProductInCart() {
+    let promiseArray = [];
 
-    for(let i=0; i<allCart.length; i++){
-        promiseArray.push(new Promise((resolve)=>{
-            fetch("http://localhost:3000/api/products/"+ allCart[i].id)
-            .then(function(respons){
-                return respons.json();
-            })
-            .then(function(cartResult){
-                const dataCart = {...cartResult,...allCart[i]};
-                /*const dataCart =[
-                    id:allCart[i].id,
-                    color: allCart[i].color,
-                    quantity: allCart[i].quantity,
-                    imageUrl
-                ]*/
-            resolve(dataCart);           
-
-            })
-
+    for (let i = 0; i < allCart.length; i++) {
+        promiseArray.push(new Promise((resolve) => {
+            fetch("http://localhost:3000/api/products/" + allCart[i].id)
+                .then(function (respons) {
+                    return respons.json();
+                })
+                .then(function (cartResult) {
+                    const dataCart = { ...cartResult, ...allCart[i] };
+                    /*const dataCart =[
+                        id:allCart[i].id,
+                        color: allCart[i].color,
+                        quantity: allCart[i].quantity,
+                        imageUrl
+                    ]*/
+                    resolve(dataCart);
+                })
         }))
-       
-        
-       
-        
     }
+
     const results = await Promise.all(promiseArray);
     return results;
-}
 
-async function displayCart(){
+}
+async function displayCart() {
     const results = await fetchProductInCart();
     allCartWithInfo = results;
     let quantity = 0;
@@ -41,42 +36,79 @@ async function displayCart(){
     let totalQuantity = document.querySelector("#totalQuantity");
     let totalPrice = document.querySelector("#totalPrice");
     const elementCart = document.querySelector("#cart__items");
-    for(let i=0; i< allCartWithInfo.length; i++){
+    for (let i = 0; i < allCartWithInfo.length; i++) {
+
         quantity += allCartWithInfo[i].quantity;
-        elementCart.innerHTML +=
-        `<article class="cart__item" data-id="${allCartWithInfo[i].id}" data-color="${allCartWithInfo[i].color}">
-        <div class="cart__item__img">
-        <img src=${allCartWithInfo[i].imageUrl} alt=${allCartWithInfo[i].altTxt}>
-        </div>
-        <div class="cart__item__content">
-        <div class="cart__item__content__description">
-            <h2>${allCartWithInfo[i].name}</h2>
-            <p>${allCartWithInfo[i].color}</p>
-            <p>${allCartWithInfo[i].price} €</p>
-        </div>
-        <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-            <p>Qté : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${allCartWithInfo[i].quantity}">
-            </div>
-            <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Supprimer</p>
-            </div>
-        </div>
-        </div>
-        </article>`;
+        price += allCartWithInfo[i].quantity * allCartWithInfo[i].price;
+        totalQuantity.innerText = `${quantity}`;
+        totalPrice.innerText = `${price}`;
+
+
+        // Déclaration de la variable articleCart en créant un article avec le parent elementCart
+        let articleCart = document.createElement('article');
+        articleCart.classList.add('cart__item');
+        elementCart.appendChild(articleCart);
+
+        // Déclaration de la variable imageDiv en créant une div avec le parent articleCart
+        let imageDiv = document.createElement('div');
+        imageDiv.classList.add('cart__item__img');
+        articleCart.appendChild(imageDiv);
+
+        // Déclaration de la variable imageProduct en créant une image avec le parent imageDiv
+        let imageProduct = document.createElement('img');
+        imageProduct.src += `${allCartWithInfo[i].imageUrl} `;
+        imageProduct.alt += `alt=${allCartWithInfo[i].altTxt}`;
+        imageDiv.appendChild(imageProduct);
+
+        // Déclaration de la variable globalDescriptionProduct en créant une div avec le parent articleCart
+        let globalDescriptionProduct = document.createElement('div');
+        globalDescriptionProduct.classList.add('cart__item__content');
+        articleCart.appendChild(globalDescriptionProduct);
+
+        let descriptionProduct = document.createElement('div');
+        descriptionProduct.classList.add('cart__item__content__description');
+        descriptionProduct.innerHTML += `<h2>${allCartWithInfo[i].name}</h2><p>${allCartWithInfo[i].color}</p><p>${allCartWithInfo[i].price} €</p>`;
+        globalDescriptionProduct.appendChild(descriptionProduct);
+
+        // Déclaration de la variable cartSetting en créant une div avec le parent globalDescriptionProduct
+        let cartSetting = document.createElement('div');
+        cartSetting.classList.add('cart__item__content__settings');
+        globalDescriptionProduct.appendChild(cartSetting);
+
+        // Déclaration de la variable inputQuantity en créant une input avec le parent cartSetting
+        let inputQuantity = document.createElement('div');
+        inputQuantity.classList.add('cart__item__content__settings__quantity');
+        inputQuantity.innerHTML += `<p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${allCartWithInfo[i].quantity}">`;
+        cartSetting.appendChild(inputQuantity);
+
+        // Déclaration de la variable inputQuantity en créant une input avec le parent cartSetting
+        let deleteCartProduct = document.createElement('div');
+        deleteCartProduct.classList.add('cart__item__content__settings__delete');
+        deleteCartProduct.innerHTML += `<p class="deleteItem">Supprimer</p>`;
+        cartSetting.appendChild(deleteCartProduct);
+        
     }
-    totalQuantity.innerText=`${quantity}`;
-    totalPrice.innerText=`${price}`;
+    
+
 }
 displayCart();
 
+/*let productDelete = document.querySelector('.cart__item__content__settings__delete');
+productDelete = productDelete.innerText;
+console.log(productDelete);
+productDelete.onclick = await function(){
+  console.log('product');  
+};*/
 
 
 
 
- 
- 
+
+
+
+
+
 
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,66 +1,80 @@
 const params = new URL(document.location).searchParams;
 const id = params.get("id")
-console.log(id);
 
-async function fetchProductSelect(){
-    await fetch("http://localhost:3000/api/products/"+ id)
-    .catch(function(error){
-        console.log(error);
-    })
-    .then(function(respons){
-        console.log(respons);
-        return respons.json();
-    })
-    .then(function(objectresult){
+async function fetchProductSelect() {
+    await fetch("http://localhost:3000/api/products/" + id)
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function (respons) {
+            console.log(respons);
+            return respons.json();
+        })
+        .then(function (objectresult) {
 
-        const imageSection = document.querySelector(".item__img");        
-        imageSection.innerHTML =`<img src=${objectresult.imageUrl} alt=${objectresult.altTxt}></img>`
-       
-        const titreSection = document.querySelector("#title");        
-        titreSection.innerText =`${objectresult.name}`;
+            const imageSection = document.querySelector(".item__img");
+            imageSection.innerHTML = `<img src=${objectresult.imageUrl} alt=${objectresult.altTxt}></img>`
 
-        const priceSection = document.querySelector("#price");        
-        priceSection.innerText =`${objectresult.price}`;
-        
-        const descriptionSection = document.querySelector("#description");        
-        descriptionSection.innerText =`${objectresult.description}`;
-        
-        const colorSection = document.querySelector("select");
+            const titreSection = document.querySelector("#title");
+            titreSection.innerText = `${objectresult.name}`;
 
-        for (let i=0; i<=objectresult.colors.length-1; i++){       
-            colorSection.innerHTML +=`<option value=${objectresult.colors[i]}>${objectresult.colors[i]}</option>`;
-        }
+            const priceSection = document.querySelector("#price");
+            priceSection.innerText = `${objectresult.price}`;
 
-        const quantityInput = document.querySelector("#quantity");
-        const addToCartButton = document.querySelector("#addToCart");    
-        addToCartButton.onclick = () =>{
-            addToCart(quantityInput.value, colorSection.value); 
-            
-        }
-    
-         
-    })
+            const descriptionSection = document.querySelector("#description");
+            descriptionSection.innerText = `${objectresult.description}`;
+
+            const colorSection = document.querySelector("select");
+
+            for (let i = 0; i <= objectresult.colors.length - 1; i++) {
+                colorSection.innerHTML += `<option value=${objectresult.colors[i]}>${objectresult.colors[i]}</option>`;
+            }
+
+            const quantityInput = document.querySelector("#quantity");
+            const addToCartButton = document.querySelector("#addToCart");
+            addToCartButton.onclick = () => {
+                addToCart(quantityInput.value, colorSection.value);
+
+            }
+        })
 }
 
-function addToCart(quantity,color){
-       const newElements ={
-                id: id,
-                quantity: parseInt(quantity),
-                color: color,
-                
+function addToCart(quantity, color) {
+    const newElements = {
+        id: id,
+        quantity: parseInt(quantity),
+        color: color,
+
+    }
+    const storedData = localStorage.getItem("elementsCart");
+    let cartElements;
+
+    if (!storedData) {
+        cartElements = [];
+    }
+    else {
+
+        cartElements = JSON.parse(storedData);
+        
+        for (let element in cartElements) {
+
+            console.log(cartElements[element]);
+            if (cartElements[element].id == newElements.id && cartElements[element].color == newElements.color) {
+                console.log(element);
+                cartElements.splice(element,1);
             }
-            const storedData = localStorage.getItem("elementsCart");
-            let cartElements;
-            if (!storedData){
-                cartElements=[];
-            }
-            else{
-                cartElements =JSON.parse(storedData)
-            }
-            cartElements.push(newElements);
-            localStorage.setItem("elementsCart",JSON.stringify(cartElements));
-            window.confirm("Produit ajouté dans la panier");
-            
+        }
+
+    }
+
+    cartElements.push(newElements);
+
+
+    localStorage.setItem("elementsCart", JSON.stringify(cartElements));
 }
+
+
+
+
 fetchProductSelect();
 
